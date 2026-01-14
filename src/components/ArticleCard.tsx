285c9051684e442e06d816b517fe6bac00c7fb4f
anisphia-palettia <euphyllia.tomodachi@gmail.com>
2026-01-14 20:30:45 +0700

    update

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -9,37 +9,42 @@ interface ArticleCardProps {
   title?: string;
   description?: string;
   image?: string;
-  slug? : string;
+  slug?: string;
 }
 
-export function ArticleCard({ title, description, image, slug }: ArticleCardProps) {
+export function ArticleCard({
+  title,
+  description,
+  image,
+  slug,
+}: ArticleCardProps) {
   return (
     <Link href={slug ? `/articles/${slug}` : "/articles/test-article"}>
-        <Card className="grid grid-cols-1 md:grid-cols-[1fr_220px] gap-6 p-6 items-stretch hover:bg-accent">
-            <div className="">
-                <Badge variant="secondary" className="w-fit mb-4">
-                    Artikel
-                </Badge>
+      <Card className="grid grid-cols-1 md:grid-cols-[1fr_220px] gap-6 p-6 items-stretch hover:bg-accent">
+        <div className="">
+          <Badge variant="secondary" className="w-fit mb-4">
+            Artikel
+          </Badge>
 
-                <TypographyH3 className="line-clamp-3">
-                    {title || LOREM_IPSUM}
-                </TypographyH3>
+          <TypographyH3 className="line-clamp-3">
+            {title || LOREM_IPSUM}
+          </TypographyH3>
 
-                <TypographyP className="mt-0 line-clamp-2">
-                    {description || LOREM_IPSUM}
-                </TypographyP>
-            </div>
+          <TypographyP className="mt-0 line-clamp-2">
+            {description || LOREM_IPSUM}
+          </TypographyP>
+        </div>
 
-            <div className="relative aspect-square w-full h-full overflow-hidden rounded-lg bg-muted">
-                <Image
-                    src={image || "/image.png"}
-                    alt="Artikel thumbnail"
-                    fill
-                    className="object-cover"
-                    sizes="220px"
-                />
-            </div>
-        </Card>
+        <div className="relative aspect-square w-full h-full overflow-hidden rounded-lg bg-muted">
+          <Image
+            src={image || "/logo.jpg"}
+            alt="Artikel thumbnail"
+            fill
+            className="object-cover"
+            sizes="220px"
+          />
+        </div>
+      </Card>
     </Link>
   );
 }
